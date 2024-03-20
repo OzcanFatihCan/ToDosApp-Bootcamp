5652f757f9e4f24dccb2bb0634088a389151c0a4
Özcan Fatih Can <ozcanfatihcann@gmail.com>
2024-03-20 21:47:29 +0300

    Feat: Added bottom navigation structure

--- app/src/main/java/com/ozcanfatihcan/todosapp/MainActivity.kt
+++ app/src/main/java/com/ozcanfatihcan/todosapp/MainActivity.kt
@@ -5,18 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ozcanfatihcan.todosapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.mainActivityObject=this
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         getWindow().setStatusBarColor(getResources().getColor(R.color.toolBar))
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.navController)
+
+
     }
 }
--- app/src/main/java/com/ozcanfatihcan/todosapp/util/Extensions.kt
+++ app/src/main/java/com/ozcanfatihcan/todosapp/util/Extensions.kt
@@ -1,3 +1,12 @@
 package com.ozcanfatihcan.todosapp.util
 
+import android.view.View
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
+fun Navigation.gecisYap(it:View,fragmentName:Int){
+    findNavController(it).navigate(fragmentName)
+}
+fun Navigation.gecisYap(it:View,fragmentName:NavDirections){
+    findNavController(it).navigate(fragmentName)
+}
