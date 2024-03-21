84fe798e8aa87e022b615b40c9ca2c6ae5ca4d8a
Özcan Fatih Can <ozcanfatihcann@gmail.com>
2024-03-21 15:30:10 +0300

    Feat: todo add page prepared

--- app/src/main/java/com/ozcanfatihcan/todosapp/ui/fragment/AddPageFragment.kt
+++ app/src/main/java/com/ozcanfatihcan/todosapp/ui/fragment/AddPageFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.ozcanfatihcan.todosapp.R
+import com.ozcanfatihcan.todosapp.databinding.FragmentAddPageBinding
 
 class AddPageFragment : Fragment() {
+    private lateinit var binding:FragmentAddPageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_page, container, false)
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_add_page,container,false)
+        binding.todoAddPage=this
+
+        return binding.root
     }
 }
